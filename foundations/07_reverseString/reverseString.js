f317d2dea67cffc1a258300a9fafaabdf7aa0e17
Abdelrahman Elkhouly <khouly.dev@gmail.com>
2026-08-19 15:38:32 +0300

    Solve 07 reverse string task

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let reversed = "";
+  // loop overy the string
+  for (let i = str.length - 1; i >= 0; i--) {
+    // pop the last character and add it to a empty string
+    reversed += str[i];
+  }
+  return reversed;
 };
 
 // Do not edit below this line
