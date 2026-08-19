cc096205c55910064e05523a676ae6eb16c9a6ac
Abdelrahman Elkhouly <khouly.dev@gmail.com>
2026-08-19 18:24:46 +0300

    Solve 09 sum all task

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (min <= 0 || max <= 0) return "EROR";
 
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
